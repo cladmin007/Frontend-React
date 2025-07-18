ec9b79cc4bb8debf51c0ab2cdb860dbdaccc7362
cladmin007 <arunkamalkavi007@gmail.com>
2025-07-18 17:39:57 +0530

    Components changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashbord from './pages/Dashbord'
+
 
 function App() {
   
-
   return (
     <div>
-      <h1>Hello Welcome</h1>
+      <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />}/>
+        <Route path='/register' element={<Register />}/>
+        <Route path='/dashboard' element={<Dashbord />}/>
+      </Routes>
+      </BrowserRouter>
     </div>
   )
 }
---
+++ src/pages/Login.tsx
@@ -0,0 +1,11 @@
+
+
+const Login = () => {
+    return (
+        <div className="flex items-center justity-cente bg-gray-100">
+            <h1 className="text-3xl">Login</h1>
+        </div>
+    )
+}
+
+export default Login
