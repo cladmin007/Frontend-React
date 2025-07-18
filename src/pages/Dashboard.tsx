534c0c0cb5d3ac33e6b61266ddee72115a7877a3
arun <arunrajith25@gmail.com>
2025-07-18 20:30:43 +0530

    App

---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,8 @@
+export const Dashboard = () => {
+  return (
+    <div>
+        <h1 className='text-2xl font-bold'>Welcome back,</h1>
+        <p className='text-red-500'>Xp:10 Streak:10 days</p>
+    </div>
+  )
+}
